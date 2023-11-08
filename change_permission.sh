fa6a1ab7c859aad418ae14b7b57d91917e2f834f
dial <dial@dialui-MacBookPro.local>
2023-11-08 15:24:19 +0900

    container name for permission changed

--- change_permission.sh
+++ change_permission.sh
@@ -4,4 +4,4 @@
 chmod -R 777 notebooks
 
 # change permission of certs at swe3003_es-jupyter-1
-docker exec -it --user root swe3003_es-jupyter-1 /bin/bash -c "chmod -R 777 certs"
+docker exec -it --user root swe3033_es-jupyter-1 /bin/bash -c "chmod -R 777 certs"
